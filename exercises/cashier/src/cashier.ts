522fd9131cc5f8dbcea763b437564312ba757ae1
Reltre <tglippert@gmail.com>
2018-03-24 12:44:16 -0700

    Update cashier function with correct return shapes and value.

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,7 +1,36 @@
+interface CartItem {
+  name: string;
+  price: number;
+  qty: number;
+}
+
+interface CartAPI {
+  add: object;
+  addItem: object;
+  length: number;
+  total: number;
+}
+
 export function cashier() {
-  return { 
-    add: function() {},
-    length: 0,
+  function updateCart(price: number, qty: number) {
+    for(let idx = 0; idx < qty; idx++) {
+      cart.total += price;
+      cart.length++;
+    }
+  }
+  
+  let cart: CartAPI = { 
+    add: function(name: string, price: number, qty: number = 1): CartAPI {
+      updateCart(price, qty);
+      return this; 
+    }, 
+    addItem: function({name, price, qty}: CartItem): CartAPI {
+      updateCart(price, qty);
+      return this; 
+    }, 
+    length: 0, 
     total: 0 
-  };
+  }
+
+  return cart;
 }
