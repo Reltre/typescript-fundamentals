61d8c9fc14b6f96e33a0c9f0b03ecb540b2d6726
Reltre <tglippert@gmail.com>
2018-04-04 16:25:54 -0700

    Simplify implementation based on generic stack solution.

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -4,91 +4,66 @@ interface IStack<T> {
   pop(): T | undefined;
   length(): number;
   print(): void;
-  head: INode<T> | null;
-  tail: INode<T> | null;
-  current: INode<T> | null;
 }
 
 interface INode<T> {
   data: T;
-  next: INode<T> | null;
-  prev: INode<T> | null;
+  next: INode<T> | undefined;
 }
 
 class Node<T> implements INode<T> {
   data: T;
-  next:  INode<T> | null;
-  prev: INode<T> | null;
+  next:  INode<T> | undefined;
 
   constructor(data: T) {
     this.data = data;
-    this.next = null;
-    this.prev = null;
   }
-
 }
 
-export class Stack<T extends String> implements IStack<T> {
-  private items: T[];
-  head: Node<T> | null;
-  tail: Node<T> | null;
-  current: Node<T> | null;
-  _length: number;
-
-  constructor(){
-    this.items = [];
-    this.head = null;
-    this.tail = null;
-    this.current = this.head;
-    this._length = 0;
-  }
+export class Stack<T> implements IStack<T> {
+  private head: Node<T> | undefined;
 
   length(): number {
-    return this._length;
+    let length = 0;
+    let node = this.head;
+    while(node) {
+      length += 1;
+      node = node.next;
+    }
+    return length;
   }
 
  
   push(item: T): Stack<T>;
-  push(item: T[]): Stack<T>;
-  push(item: T| T[]): Stack<T> {
-    if (typeof item === "string") {
-      this.setNode(item);
-      this._length += 1;
-    } else if (typeof item === "object") {
-      item.forEach((obj: T) => { 
-        this.setNode(obj);
-        this._length += 1;
-      });
+  push(items: T[]): Stack<T>;
+  push(itemOrArray: T| T[]): Stack<T> {
+    if (itemOrArray instanceof Array) {
+      itemOrArray.forEach((obj: T) => this.push(obj) );
+    } else {
+      this.setNode(itemOrArray);
     }
     return this;
   }
 
   pop(): T | undefined {
-    if (this.current !== null && this.current.prev !== null) { 
-      let item = this.current.data;
-      this.current = this.current.prev;
-      this.current.next = null;
-      this._length -= 1;
-      return item;
-    } 
+    if (!this.head) return undefined; 
+
+    let item = this.head.data;
+    this.head = this.head.next;
+    return item;
   }
 
   print(): void {
-    console.log(this);
+    let node = this.head;
+    while(node) {
+      console.log(node.data);
+      node = node.next;
+    }
   }
 
   private setNode(item: T) {
-    if (this.head === null) {
-      this.head = new Node(item);
-      this.current = this.head;
-    } else if (this.head !== null &&  this.head.next === null) {
-      this.current = new Node(item);
-      this.head.next = this.current;
-      this.current.prev = this.head;
-    } else if (this.current !== null && this.head.next !== null) {
-      this.current.next = new Node(item);
-      this.current.next.prev = this.current;
-      this.current = this.current.next;
-    }
+    let node = new Node(item);
+    node.next = this.head;
+    this.head = node;
   }
 }
