d450a029fd826923b62f6af951111964a284ab00
Reltre <tglippert@gmail.com>
2018-04-10 00:04:12 -0700

    Initial implementation of fibonacci generator function.

--- exercises/fib/src/fib.ts
+++ exercises/fib/src/fib.ts
@@ -3,6 +3,14 @@
  * Implement a generator function that can be used
  * to generate numbers in the Fibonacci Sequence
  */
-// export function* getFibSequence(): IterableIterator<number> {
- 
-// }
+export function* getFibSequence(): IterableIterator<number> {
+  let first = 0;
+  let second = 1;
+  let result;
+  while(true) {
+    result = first + second;
+    yield result;
+    second = first;
+    first = result;
+  }
+}
